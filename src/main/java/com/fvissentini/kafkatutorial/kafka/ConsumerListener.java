bd1f292f4cb5b1f12fe1e4f1b2ae7c2c7c0502c0
fernandovissentini <fvissentini@gmail.com>
2023-01-02 20:33:01 +0100

    Create consumer

---
+++ src/main/java/com/fvissentini/kafkatutorial/kafka/ConsumerListener.java
@@ -0,0 +1,18 @@
+package com.fvissentini.kafkatutorial.kafka;
+
+import static com.fvissentini.kafkatutorial.config.KafkaTopicConfig.TOPIC_NAME;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class ConsumerListener {
+
+  @KafkaListener(topics = TOPIC_NAME, groupId = "myGroup")
+  public void listenTopic(String message) {
+    log.info("Message reveived: {}", message);
+  }
+
+}
